2aa0aee58b9f2f0a3daaba3e83e39de203e3353a
unknown <renzocretecio@gmail.com>
2020-04-03 23:02:08 +0800

    form control

--- src/app/content/contact-me/contact-me.component.ts
+++ src/app/content/contact-me/contact-me.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { ContactForm } from './ObjectModel'
 import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import * as moment from 'moment';
@@ -10,56 +10,48 @@ import * as $ from 'jquery'
   styleUrls: ['./contact-me.component.css']
 })
 export class ContactMeComponent implements OnInit {
-  contactFormObject: ContactForm
-
-  contactForm = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', Validators.required],
-    message: ['']
-
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    message: new FormControl(''),
   })
 
+
+
   onSubmit() {
-    $('#contact').on('click', () => {
-      $('#submit-form').submit()
-    })
+    const body = new HttpParams()
+      .set('form-name', 'contact')
+      .append('name', this.contactForm.value.name)
+      .append('name', this.contactForm.value.email)
+      .append('name', this.contactForm.value.message)
+    this.http.post('/', body.toString(), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).subscribe(
+      res => { },
+      err => {
+        if (err instanceof ErrorEvent) {
+          //client side error
+          alert("Something went wrong when sending your message.");
+          console.log(err.error.message);
+        } else {
+          //backend error. If status is 200, then the message successfully sent
+          if (err.status === 200) {
+            alert("Your message has been sent!");
+          } else {
+            alert("Something went wrong when sending your message.");
+            console.log('Error status:');
+            console.log(err.status);
+            console.log('Error body:');
+            console.log(err.error);
+          };
+        };
+      }
+    )
+
   }
-
-  // onSubmit() {
-  //   const body = new HttpParams()
-  //     .set('form-name', 'contact')
-  //     .append('name', this.contactForm.value.name)
-  //     .append('name', this.contactForm.value.email)
-  //     .append('name', this.contactForm.value.message)
-  //   this.http.post('/', body.toString(), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).subscribe(
-  //     res => { },
-  //     err => {
-  //       if (err instanceof ErrorEvent) {
-  //         //client side error
-  //         alert("Something went wrong when sending your message.");
-  //         console.log(err.error.message);
-  //       } else {
-  //         //backend error. If status is 200, then the message successfully sent
-  //         if (err.status === 200) {
-  //           alert("Your message has been sent!");
-  //         } else {
-  //           alert("Something went wrong when sending your message.");
-  //           console.log('Error status:');
-  //           console.log(err.status);
-  //           console.log('Error body:');
-  //           console.log(err.error);
-  //         };
-  //       };
-  //     }
-  //   )
-
-  // }
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
-    this.onSubmit()
   }
 
 }
